8c1b018451eaf47b9ccc6e1f65c65d27259d466d
dimples <13650125+dimples-app@users.noreply.github.com>
2021-04-30 13:55:50 -0700

    Create Components

---
+++ src/pomodoro/DurationFocus.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function DurationFocus(props) {
+  return (
+    <div className="input-group input-group-lg mb-2">
+      <span className="input-group-text" data-testid="duration-focus">
+        {/* TODO: Update this text to display the current focus session duration */}
+        Focus Duration: 25:00
+      </span>
+      <div className="input-group-append">
+        {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid="decrease-focus"
+        >
+          <span className="oi oi-minus" />
+        </button>
+        {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-testid="increase-focus"
+        >
+          <span className="oi oi-plus" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default DurationFocus;
