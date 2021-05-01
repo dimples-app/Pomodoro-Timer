5c2737dd7bbdc8ef8dbb9ae9e9d56b7697732846
dimples <13650125+dimples-app@users.noreply.github.com>
2021-04-30 18:13:47 -0700

    Add hooks and handler

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -9,6 +9,58 @@ function Pomodoro() {
   // Timer starts out paused
   const [isTimerRunning, setIsTimerRunning] = useState(false);
 
+  // set up focus/ break duration
+  const [focusDuration, setFocusDuration] = useState(25); // in minute
+  const [breakDuration, setBreakDuration] = useState(5); //in minute
+
+  // adjust session active default to inactive
+  const [isSessionActive, setIsSessionActive] = useState(false);
+
+  //adjust pause default to pause
+  const [isSessionPause, setIsSessionPause] = useState(true);
+
+  //set up timer remaining
+  const [timer, setTimer] = useState(1500); //in second
+
+  // current session default to "Focus"
+  const [currentState, setCurrentState] = useState("focus");
+
+  // handle increment on focus
+  const handleFocusIncrement = () => {
+    setFocusDuration((currentValue) => {
+      return Math.min(60, currentValue + 5);
+    });
+  };
+
+  //handle decrement on focus
+  const handleFocusDecrement = () => {
+    setFocusDuration((currentValue) => {
+      return Math.max(5, currentValue - 5);
+    });
+  };
+
+  //handle inrement on break
+  const handleBreakIncrement = () => {
+    setBreakDuration((currentValue) => {
+      return Math.min(15, currentValue + 1);
+    });
+  };
+
+  //handle decrement on break
+  const handleBreakDecrement = () => {
+    setBreakDuration((currentValue) => {
+      return Math.max(1, currentValue - 1);
+    });
+  };
+
+  //handle stop button
+  const handleStopButton = () => {
+    setIsSessionActive(false);
+    setIsTimerRunning(false);
+    setIsSessionPause(true);
+    setCurrentState("focus");
+  };
+
   useInterval(
     () => {
       // ToDo: Implement what should happen when the timer is running
