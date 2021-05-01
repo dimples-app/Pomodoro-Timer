372ad84cfa376292ff3439f9d0031f28b77aef89
dimples <13650125+dimples-app@users.noreply.github.com>
2021-04-30 18:15:21 -0700

    Add Props and propstype to component

--- src/pomodoro/DurationBreak.js
+++ src/pomodoro/DurationBreak.js
@@ -1,11 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { minutesToDuration } from "../utils/duration";
 
 function DurationBreak(props) {
+  const {
+    breakDuration,
+    handleBreakDecrement,
+    handleBreakIncrement,
+    isSessionActive,
+  } = props;
+
   return (
     <div className="input-group input-group-lg mb-2">
       <span className="input-group-text" data-testid="duration-break">
         {/* TODO: Update this text to display the current break session duration */}
-        Break Duration: 05:00
+        Break Duration: {minutesToDuration(breakDuration)}
       </span>
       <div className="input-group-append">
         {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
@@ -13,6 +22,8 @@ function DurationBreak(props) {
           type="button"
           className="btn btn-secondary"
           data-testid="decrease-break"
+          onClick={!isSessionActive ? handleBreakDecrement : undefined}
+          disabled={isSessionActive}
         >
           <span className="oi oi-minus" />
         </button>
@@ -21,6 +32,8 @@ function DurationBreak(props) {
           type="button"
           className="btn btn-secondary"
           data-testid="increase-break"
+          onClick={!isSessionActive ? handleBreakIncrement : undefined}
+          disabled={isSessionActive}
         >
           <span className="oi oi-plus" />
         </button>
@@ -28,5 +41,12 @@ function DurationBreak(props) {
     </div>
   );
 }
+
+DurationBreak.propTypes = {
+  breakDuration: PropTypes.number.isRequired,
+  handleBreakDecrement: PropTypes.func.isRequired,
+  handleBreakIncrement: PropTypes.func.isRequired,
+  isSessionActive: PropTypes.bool.isRequired,
+};
 
 export default DurationBreak;
