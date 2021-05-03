69f16998f5f845e902fa21f8da00eb3f8e11ce69
dimples <13650125+dimples-app@users.noreply.github.com>
2021-05-03 10:54:18 -0700

    Add logic for ToDo list

--- src/pomodoro/ToDo.js
+++ src/pomodoro/ToDo.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function ToDo() {
-  return <div></div>;
+function Task({ task, index, completeTask, removeTask }) {
+  return (
+    <div
+      className="task"
+      style={{ textDecoration: task.completed ? "line-through" : "" }}
+    >
+      {task.title}
+
+      <button style={{ background: "gray" }} onClick={() => removeTask(index)}>
+        x
+      </button>
+      <button onClick={() => completeTask(index)}>Complete</button>
+    </div>
+  );
 }
 
-export default ToDo;
+function CreateTask({ addTask }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTask(value);
+    setValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add a new task"
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function Todo() {
+  const [tasksRemaining, setTasksRemaining] = useState(0);
+  const [tasks, setTasks] = useState([
+    {
+      title: "Code Review",
+      completed: false,
+    },
+  ]);
+
+  useEffect(() => {
+    setTasksRemaining(tasks.filter((task) => !task.completed).length);
+  }, [tasks]);
+
+  const addTask = (title) => {
+    const newTasks = [...tasks, { title, completed: false }];
+    setTasks(newTasks);
+  };
+
+  const completeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = true;
+    setTasks(newTasks);
+  };
+
+  const removeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div className="todo-container">
+      <div className="header">List Of Tasks ({tasksRemaining})</div>
+      <div className="tasks">
+        {tasks.map((task, index) => (
+          <Task
+            task={task}
+            index={index}
+            completeTask={completeTask}
+            removeTask={removeTask}
+            key={index}
+          />
+        ))}
+      </div>
+      <div className="create-task">
+        <CreateTask addTask={addTask} />
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
