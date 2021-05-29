9f8bedea29e7673b7fd5555e6bb3957c8d9694e6
ddoshi2021 <ddoshi2021@gmail.com>
2021-05-29 13:33:47 -0700

    Add files

---
+++ build/precache-manifest.537206ef39263f34182b03ab6ff429fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d517dee57a773aef0d1dd99134ff0c2",
+    "url": "/Pomodoro-Timer/index.html"
+  },
+  {
+    "revision": "51433f4cea0d0a5447bf",
+    "url": "/Pomodoro-Timer/static/css/main.3202b0c8.chunk.css"
+  },
+  {
+    "revision": "1965870259743c439629",
+    "url": "/Pomodoro-Timer/static/js/2.01fc0311.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro-Timer/static/js/2.01fc0311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51433f4cea0d0a5447bf",
+    "url": "/Pomodoro-Timer/static/js/main.d48cdab2.chunk.js"
+  },
+  {
+    "revision": "fa519a5d7be288e60c37",
+    "url": "/Pomodoro-Timer/static/js/runtime-main.3e9ec4f7.js"
+  }
+]);
--- src/pomodoro/DurationFocus.js
+++ src/pomodoro/DurationFocus.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { minutesToDuration } from "../utils/duration";
 /**
  * function/ comp for focus duration
  * @param {*} props 
